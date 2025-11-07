b5580191fbc44aeee38be8125991ac5d5fd39836
Anne MAVRO <anne.mavro@gmail.com>
2025-11-07 15:27:34 +0100

    Add pages structure and update routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App';
+import Home from './pages/Home';
+import Checkin from './pages/Checkin';
+import Chat from './pages/Chat';
+import Exercises from './pages/Exercises';
+import Journal from './pages/Journal';
+import Resources from './pages/Resources';
+import Settings from './pages/Settings';
+
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "checkin", element: <Checkin /> },
+      { path: "chat", element: <Chat /> },
+      { path: "exercises", element: <Exercises /> },
+      { path: "journal", element: <Journal /> },
+      { path: "resources", element: <Resources /> },
+      { path: "settings", element: <Settings /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
